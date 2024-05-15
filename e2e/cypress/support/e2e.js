a65038f0d9197bbd794e710356987f5d95f2eb39
viniciuswdmf <vinicius.jacques@compasso.com.br>
2024-05-15 15:06:46 -0300

    atualizando cy.yml

--- e2e/cypress/support/e2e.js
+++ e2e/cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import 'cypress-mochawesome-reporter/register'
 import './commands'
+import 'cypress-mailosaur'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
